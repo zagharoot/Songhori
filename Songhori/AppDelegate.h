03aa10608f6402d9c6112de6b58576182b010cc5
Ali Nouri <nouri@cs.rutgers.edu>
2011-12-02 13:08:45 -0500

    Initial Commit

---
+++ Songhori/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Songhori
+//
+//  Created by Ali Nouri on 12/2/11.
+//  Copyright (c) 2011 Rutgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Songhori/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Songhori
+//
+//  Created by Ali Nouri on 12/2/11.
+//  Copyright (c) 2011 Rutgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
